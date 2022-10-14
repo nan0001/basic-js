d793a85c3afd66b32b140734bd27a26a8bb5cacf
Nastya <himeriya97@mgmail.com>
2022-10-14 12:58:10 +0300

    feat: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,15 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  let t = 0;
+  if (Number(sampleActivity) && Number(sampleActivity) > 0 && typeof sampleActivity === 'string' && Number(sampleActivity) < 15){
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    t = Math.log(MODERN_ACTIVITY / sampleActivity) / k;
+    return Math.ceil(t)
+  } else {
+    return false
+  }
 }
 
 module.exports = {
