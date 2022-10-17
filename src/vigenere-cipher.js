0f48c593e7d5f91934dfc584ebd912d2c166fd69
Nastya <himeriya97@mgmail.com>
2022-10-17 18:45:58 +0300

    feat: viginere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,102 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
-class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+ class InvalidArgs extends Error {
+  constructor(message) {
+      super(message);
+      this.name = 'InvalidArgs'
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+};
+
+class VigenereCipheringMachine {
+
+  constructor(direct = true){
+    this.direct = direct
+  }
+
+  encrypt(message, key) {
+    if (!message || !key){
+      throw new InvalidArgs('Incorrect arguments!');
+    }
+
+    let alpha = 'abcdefghijklmnopqrstuvwxyz';
+    let keyPhrase = '';
+    let indInKey = 0;
+    for (let i = 0; i < message.length; i++){
+      if (/[a-z]/i.test(message[i])){
+        if (indInKey >= key.length){
+          indInKey -= key.length;
+        }
+        keyPhrase += key[indInKey];
+        indInKey++;
+      } else {
+        keyPhrase += message[i]
+      }
+    }
+
+    let encrypted = '';
+    for (let i = 0; i < keyPhrase.length; i++){
+      if (/[a-z]/i.test(keyPhrase[i])){
+        let ind = alpha.indexOf(message[i].toLowerCase()) + alpha.indexOf(keyPhrase[i].toLowerCase());
+        if (ind > 25){
+          ind -= 26;
+        }
+        let letter = alpha[ind];
+        encrypted += letter
+      } else {
+        encrypted += keyPhrase[i]
+      }
+    }
+
+    if (this.direct){
+      return encrypted.toUpperCase()
+    } else{
+      return encrypted.toUpperCase().split('').reverse().join('')
+    } 
+  }
+
+  decrypt(message, key) {
+    if (!message || !key){
+      throw new InvalidArgs('Incorrect arguments!');
+    }
+
+    let alpha = 'abcdefghijklmnopqrstuvwxyz';
+    let keyPhrase = '';
+    let indInKey = 0;
+    for (let i = 0; i < message.length; i++){
+      if (/[a-z]/i.test(message[i])){
+        if (indInKey >= key.length){
+          indInKey -= key.length;
+        }
+        keyPhrase += key[indInKey];
+        indInKey++;
+      } else {
+        keyPhrase += message[i]
+      }
+    }
+
+    let encrypted = '';
+    for (let i = 0; i < keyPhrase.length; i++){
+      if (/[a-z]/i.test(keyPhrase[i])){
+        let ind = alpha.indexOf(message[i].toLowerCase()) - alpha.indexOf(keyPhrase[i].toLowerCase());
+        if (ind > 25){
+          ind -= 26;
+        } else if (ind < 0){
+          ind += 26
+        }
+        let letter = alpha[ind];
+        encrypted += letter
+      } else {
+        encrypted += keyPhrase[i]
+      }
+    }
+
+    if (this.direct){
+      return encrypted.toUpperCase()
+    } else{
+      return encrypted.toUpperCase().split('').reverse().join('')
+    } 
   }
 }
 
